4d6af1b9edcfe091a016bb600a089d8c34e4f26d
oqvine <oqvine@live.com>
2024-11-21 17:14:06 -0300

    Commit para incluir atividade de Boxing

---
+++ src/BoxingClass.java
@@ -0,0 +1,25 @@
+public class BoxingClass {
+
+    public static void main (String args[]) {
+
+        /**
+         * Realizando a conversão com valores inteiros
+         */
+
+        int a = 10;
+        System.out.println(a);
+
+        Integer b = a;
+        System.out.println(b);
+
+        /**
+         * Realizando a conversão com valores booleanos
+         */
+
+        boolean c = true;
+        System.out.println(c);
+
+        Boolean d = c;
+        System.out.println(d);
+    }
+}
